42246f9a2be8be0f420d2b51b7b81f8ff4cb361f
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-05-31 16:30:38 +0900

    no eval

--- Contents/Resources/CA4085CA-8C9A-4A59-B0EF-B3ACF6150382/prettyjson.pl
+++ Contents/Resources/CA4085CA-8C9A-4A59-B0EF-B3ACF6150382/prettyjson.pl
@@ -6,6 +6,7 @@ use FindBin;
 use lib "$FindBin::RealBin/lib";
 use JSON qw(from_json to_json);
 
-eval { print to_json(from_json(<STDIN>, {allow_nonref=>1}), {pretty=>1}) }
+
+print to_json(from_json(join('', <STDIN>), {allow_nonref=>1}), {pretty=>1})
 
 
